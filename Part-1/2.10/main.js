ed5e97052ba98ba89e68b3d6c81cb85349ca2722
CourageSean <seancourage23@gmail.com>
2021-02-07 22:32:00 +0100

    js

--- Part-1/2.10/main.js
+++ Part-1/2.10/main.js
@@ -52,11 +52,28 @@ let result = a + b < 4 ? "Below" : "Over";
 //   message = '';
 // }
 
-let message =
-  login == "Employee"
-    ? "Hello"
-    : login == "Director"
-    ? "Greetings"
-    : login == ""
-    ? "No login"
-    : "";
+// if the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”.
+
+// The password is checked as follows:
+
+// If it equals “TheMaster”, then show “Welcome!”,
+// Another string – show “Wrong password”,
+// For an empty string or cancelled input, show “Canceled”
+
+let userName = prompt("Who's there?", "");
+
+if (userName === "Admin") {
+  let pass = prompt("Password?", "");
+
+  if (pass === "TheMaster") {
+    alert("Welcome!");
+  } else if (pass === "" || pass === null) {
+    alert("Canceled");
+  } else {
+    alert("Wrong password");
+  }
+} else if (userName === "" || userName === null) {
+  alert("Canceled");
+} else {
+  alert("I don't know you");
+}
